344c5133c3de24feb077534091ef49ce4bb6639a
francescomascellino <franesco.mascellino@gmail.com>
2023-08-24 12:34:49 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Ciao Visitatore!
+Imposta il nome di colore rosso in vari modi:
+Solo con HTML+CSS
+Scrivendo su element.style
+Aggiungendo la classe con JS */
+
+const domTitle = document.querySelector("h1");
+
+console.log(domTitle);
+
+// domTitle.style.color = "blue";
+
+domTitle.classList.add("violet");
